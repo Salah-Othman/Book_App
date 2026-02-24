b0f2e158963766593093bfd502979feb6a49f4fd
Salah Othman <Salahothmansaad@gmail.com>
2026-02-24 13:49:51 +0200

    Add BookScreen and ListAllbook components with UI and state management integration

--- lib/features/ui/book_screen.dart
+++ lib/features/ui/book_screen.dart
@@ -1,10 +1,48 @@
+import 'package:books_app/core/constants/app_colors.dart';
+import 'package:books_app/shared/custom_text.dart';
+import 'package:books_app/shared/list_allbook.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BookScreen extends StatelessWidget {
   const BookScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.backgroundColor,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            color: AppColors.primaryColor,
+            size: 20.sp,
+          ),
+        ),
+        title: CustomText(
+          text: 'Books',
+          size: 16.sp,
+          weight: FontWeight.w600,
+          color: AppColors.primaryColor,
+        ),
+        centerTitle: true,
+      ),
+
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 21.5.h),
+
+            ListAllbook(),
+            SizedBox(height: 21.5.h),
+            ListAllbook(),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/shared/list_allbook.dart
@@ -0,0 +1,118 @@
+import 'package:books_app/core/constants/app_colors.dart';
+import 'package:books_app/features/logic/book/book_cubit.dart';
+import 'package:books_app/features/logic/book/book_state.dart';
+import 'package:books_app/features/ui/details_screen.dart';
+import 'package:books_app/shared/custom_text.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ListAllbook extends StatelessWidget {
+  const ListAllbook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => BookCubit(Dio())..getAllBooks(),
+      child: BlocBuilder<BookCubit, BookState>(
+        builder: (context, state) {
+          if (state is BookLoading) {
+            return Center(child: Text('Loading ... '));
+          } else if (state is BookSuccess) {
+            final allBooks = state.books;
+            return SizedBox(
+              height: 294.h,
+              child: ListView.separated(
+                separatorBuilder: (context, index) => SizedBox(width: 16.w),
+                scrollDirection: Axis.horizontal,
+                itemCount: allBooks.length,
+                itemBuilder: (context, index) {
+                  final allbook = allBooks[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (c) => DetailsScreen(
+                            image: allbook.cover,
+                            title: allbook.title,
+                            description: allbook.description,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      width: 180.w,
+                      height: 288.h,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Color.fromRGBO(184, 184, 184, 1),
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(top: 10),
+                            width: 180.w,
+                            height: 140.h,
+                            child: Center(child: Image.network(allbook.cover)),
+                          ),
+                          Container(
+                            width: 180.w,
+                            height: 154.h,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: AppColors.primaryColor,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CustomText(
+                                    text: 'Classics',
+                                    size: 11.sp,
+                                    weight: FontWeight.w300,
+                                    color: AppColors.secondryColor,
+                                  ),
+                                  CustomText(
+                                    text: allbook.title,
+                                    size: 15.sp,
+                                    weight: FontWeight.w600,
+                                    color: AppColors.secondaryText,
+                                  ),
+                                  SizedBox(height: 6.h),
+                                  CustomText(
+                                    text: 'Oscar Wilde',
+                                    size: 11.sp,
+                                    weight: FontWeight.w400,
+                                    color: AppColors.secondryColor,
+                                  ),
+                                  SizedBox(height: 7.h),
+                                  CustomText(
+                                    text: '\$33.00',
+                                    size: 21.sp,
+                                    weight: FontWeight.w600,
+                                    color: AppColors.secondaryText,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          } else if (state is BookError) {
+            return Center(child: Text(state.message));
+          } else {
+            return Center(child: Text('Error Load data'));
+          }
+        },
+      ),
+    );
+  }
+}
